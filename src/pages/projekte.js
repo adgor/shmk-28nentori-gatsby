dfde6df18303b801b37b407731fa16c1d22ac0d8
adgor <adgor000@gmail.com>
2021-03-29 17:48:23 +0200

    added page query for projects

--- src/pages/projekte.js
+++ src/pages/projekte.js
@@ -1,12 +1,39 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 
-export default function Projekte() {
+export default function Projekte({ data }) {
+  const projects = data.allMarkdownRemark.nodes
   return (
     <Layout>
       <div className="p-12 text-4xl bg-biscay-400 text-laRioja-500 ">
         Hello world!
       </div>
+      <div>
+        {projects.map(project => (
+          <Link to={"/projects/" + project.frontmatter.year} key={project.id}>
+            <div>
+              <h3>{project.frontmatter.title}</h3>
+              <p>{project.frontmatter.year}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
     </Layout>
   )
 }
+
+// export page query
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/(projects)/" } }) {
+      nodes {
+        frontmatter {
+          title
+          year
+        }
+        id
+      }
+    }
+  }
+`
